97a9d886be92a9de4763038f2c6d5064908cebaf
Megaladon <stanko.demon@gmail.com>
2022-10-22 18:47:55 +0300

    Semenar_2

--- FirstTask.java
+++
@@ -1,8 +0,0 @@
-public class FirstTask {
-    public static void main(String[] args) {
-        String jsonString = "{ \"фамилия \": \"Иванов \", \"оценка \": \"5 \", \"предмет \": \"Математика \"}";
-        JSONObject obj = new JSONObject(jsonString);
-        
-    }
-    
-}
